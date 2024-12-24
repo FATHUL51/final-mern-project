a5b6f175376da6b71c4ebc3d01cfd95a3b077a5b
Sk Fathul Islam <178652788+FATHUL51@users.noreply.github.com>
2024-12-24 21:33:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,11 @@ import { BrowserRouter } from "react-router-dom";
 import UserContext from "./context/UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  
     <UserContext>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </UserContext>
-  </StrictMode>
+
 );
